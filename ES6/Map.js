0577fb4edcf3492c72b27454069ed3539b57effd
intersiot <75784873+intersiot@users.noreply.github.com>
2023-03-04 15:56:22 +0900

    Update Map.js
    Map 자료형을 한 번에 만들고 싶을 때 (자료형에 직접 자료를 집어넣을 때)(

--- ES6/Map.js
+++ ES6/Map.js
@@ -16,6 +16,13 @@ person.get('age'); // map에서 자료 꺼내는 법
 person.delete('age'); // 자료 삭제하는 법
 person.size; // 자료 갯수 세는 법
 
+
+// Map 자료를 한 번에 다 만들고 싶다면 (자료형에 직접 자료를 집어넣을 때)
+var person = new Map([
+  ['name', 'kim'],
+  ['age', 20],
+]);
+
 // 반복문으로 Map에서 자료 꺼내는 법
 for (var key of person.keys()) {
   console.log(key);
