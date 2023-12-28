cb1c11cee3db56a2ae63c1e29229e5048402e9d7
intersiot <75784873+intersiot@users.noreply.github.com>
2023-12-28 19:53:58 +0900

    Create 019.js
    반복문 while

---
+++ Javascript/Recipe/019.js
@@ -0,0 +1,11 @@
+/**
+ * while문은 조건을 만족하면 계속 반복 작업을 한다.
+ * for문과 유사하나 while문은 반복 조건만을 지정한다.
+ * 그러므로 반복의 종료 시점을 지정해야 한다.
+ */
+
+let number = 0;
+while (number < 10) {
+    console.log(number);
+    number += 1;
+}
